f1e0a93bb146c9881bd5221d9505dac0223d5549
Brennan <merrett.b@gmail.com>
2022-05-20 13:53:46 -0400

    install jest

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
-
+const PORT = process.env.PORT || 3001;
 const app = express();
+const fs = require('fs');
 
-app.listen(1024, () => {
-    console.log(`API server now on port 1024!`);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/public/index.html', (req, res) => {
+  res.send('Hello!');
+});
+
+app.get('/public/notes.html', (req, res) => {
+  res.send('Hello!');
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
   });
