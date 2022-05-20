ec5c53a19c1a0978169c0b881a662a12e65168d2
Brennan <merrett.b@gmail.com>
2022-05-20 14:34:13 -0400

    began adding create note function

--- server.js
+++ server.js
@@ -15,6 +15,21 @@ app.get('/public/notes.html', (req, res) => {
   res.send('Hello!');
 });
 
+app.post('/public/index.html', (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+});
+
+function createNewNote(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
+}
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
   });
